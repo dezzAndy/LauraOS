cbf288a6609a814be90f970e7f991e1280bd9f27
André <andy.lyzar@gmail.com>
2025-08-18 23:20:14 -0600

    lauraos.py can get number of processes

--- lauraos.py
+++ lauraos.py
@@ -1,2 +1,11 @@
-num_procesos = input("Cuantos procesos quieres iniciar?\n>")
-print(int(num_procesos)*2)
+# ----------------------------
+#           LauraOS
+# Este programa es una simulación
+# del funcionamiento de un SO
+
+# To do: Hacer una pantalla de presentación
+
+# Pedimos la cantidad de procesos a ejecutar
+num_procesos = int(input("Cuantos procesos quieres iniciar?\n>"))
+print(f"Vas a ejecutar {num_procesos} procesos.")
+

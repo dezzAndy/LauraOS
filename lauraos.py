e537280bc3b997678249e1871bad9468981b8e14
André <andy.lyzar@gmail.com>
2025-08-25 14:51:14 -0600

    Logo implementation, first two imputs validation

--- lauraos.py
+++ lauraos.py
@@ -5,17 +5,32 @@
 
 # To do: Hacer una pantalla de presentación
 
+import os
+import math
+from logo import logo
+
+logo()
+
 input_valido = False
-while input_valido != False:
+while input_valido == False:
     # Pedimos la cantidad de procesos a ejecutar
+    os.system('cls')
     num_procesos = int(input("Cuantos procesos quieres iniciar?\n>"))
     print(f"Vas a ejecutar {num_procesos} procesos.")
 
     # Cada lote está compuesto por 4 procesos
 
     # Definir los lotes en objetos y contarlos.
-    if num_procesos != 0:
-        num_lotes = num_procesos/4
+    if num_procesos > 0:
+        num_lotes = math.ceil(num_procesos/4)
+        if num_lotes > 1:
+            print(f"Los procesos se han separado en {num_lotes} lotes.")
+        else:
+            print(f"Se ejecutarán en {num_lotes} lote.")
+        input_valido = True
     else:
         # Repite el ciclo
+        print("Entrada no válida.")
         input_valido = False
+
+# Definición de la clase Lote
